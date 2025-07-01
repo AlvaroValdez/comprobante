cef6c9d5c731eb7b01aa13b2b2acf7492c022d28
Alvaro Valdez <v.alvaro.r@gmail.com>
2025-07-01 14:11:47 -0400

    afinacion

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/comprobante/',  // <--- Agrega esta línea
+  base: '/',  // <--- Agrega esta línea
 })
